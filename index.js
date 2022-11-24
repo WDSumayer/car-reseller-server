e2386af760a4653292548af69548c1ff400118f0
unknown <sumairbinkalam20@gmail.com>
2022-11-24 23:58:44 +0600

    order route added

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ async function run() {
     try{
         const categoryCollections = client.db('car-Reseller').collection('categories')
         const carsCollections = client.db('car-Reseller').collection('cars')
+        const userCollections = client.db('car-Reseller').collection('users')
+        const orderCollections = client.db('car-Reseller').collection('orders')
 
         app.get('/categories', async(req, res) => {
             const query = {}
@@ -35,11 +37,32 @@ async function run() {
             const cars = await carsCollections.find(query).toArray()
             res.send(cars)
         })
-        app.post('/cars', async(req, res) => {
-            const car = req.body
-            const result = await carsCollections.insertOne(car)
+       app.put('/users/:email', async(req, res) => {
+        const email = req.params.email
+        const user = req.body
+        const query = {email: email}
+        const options = {upsert: true}
+        const updatedDoc = {
+            $set: user
+        }
+        const result = await userCollections.updateOne(query, updatedDoc, options)
+        res.send(result)
+       })
+       app.post('/orders/:id', async(req, res) => {
+            const id = req.params.id
+            const order = req.body
+            const email = order.email
+            const query = {email: email}
+            const booked = await orderCollections.find(query).toArray()
+            console.log(booked)
+            const band = booked.map(book => book._id === id)
+            if(band.length){
+                const message = `you have already booked`
+                return res.send({acknowledged: false, message})
+            }
+            const result = await orderCollections.insertOne(order)
             res.send(result)
-        })
+       })
 
     }
     finally{
