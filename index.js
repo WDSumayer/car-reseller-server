fa9c31575a11600e5590b7328aac8f9a71241bce
unknown <sumairbinkalam20@gmail.com>
2022-11-26 23:49:28 +0600

    advertise route added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -23,6 +24,8 @@ async function run() {
         const carsCollections = client.db('car-Reseller').collection('cars')
         const userCollections = client.db('car-Reseller').collection('users')
         const orderCollections = client.db('car-Reseller').collection('orders')
+        const paymentCollections = client.db('car-Reseller').collection('payments')
+        const addvertiseCollections = client.db('car-Reseller').collection('advertises')
 
         app.get('/categories', async(req, res) => {
             const query = {}
@@ -50,6 +53,12 @@ async function run() {
             const query = {email: email}
             const orders = await orderCollections.find(query).toArray()
             res.send(orders)
+        })
+        app.get('/orders/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const order = await orderCollections.findOne(query)
+            res.send(order)
         })
 
         app.get('/user/status/:email', async(req, res) => {
@@ -85,9 +94,55 @@ async function run() {
             const buyers = await userCollections.find(query).toArray()
             res.send(buyers)
         })
+
+        app.get('/addvertises', async(req, res) => {
+            const query = {}
+            const advgProducts = await addvertiseCollections.find(query).toArray()
+            res.send(advgProducts)
+        })
+
         app.post('/cars', async(req, res) => {
             const car = req.body
             const result = await carsCollections.insertOne(car)
+            res.send(result)
+        })
+
+        app.post('/create-payment-intent', async(req, res) => {
+            const order = req.body
+            const price = order.price
+            const amount = price * 100
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                  ],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+        })
+
+        app.put('/payments', async(req, res) => {
+            const payment = req.body
+            const id = payment.orderId
+            const carId = payment.car_id
+            const result = await paymentCollections.insertOne(payment)
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true}
+            const updatedDoc = {
+                $set: {
+                    status: "Paid"
+                }
+            }
+            const upadatedResult = await orderCollections.updateOne(filter, updatedDoc, options)
+
+            const query = {_id: ObjectId(carId)}
+           const carUpdatedResult = await carsCollections.updateOne(query, updatedDoc, options)
+
+           const advgQuery = {car_id: carId}
+           const advgUpdatedResult = await addvertiseCollections.updateOne(advgQuery, updatedDoc, options)
             res.send(result)
         })
 
@@ -125,6 +180,19 @@ async function run() {
                 $set: order
             }
             const result = await orderCollections.updateOne(query, updatedDoc, options)
+            res.send(result)
+       })
+       app.put('/addvertise', async(req, res) => {
+           
+            const advgProduct = req.body
+            const email = advgProduct.email
+            const id = advgProduct.car_id
+            const query = {email, car_id: id}
+            const options = {upsert: true}
+            const updatedDoc = {
+                $set: advgProduct
+            }
+            const result = await addvertiseCollections.updateOne(query, updatedDoc, options)
             res.send(result)
        })
        app.delete('/users/:id', async(req, res) => {
